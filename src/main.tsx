5f18c82b62b3e2507b9697635fe44b2ba7dbe8cf
Pedro Arthur Barrantes <pearbarrantes@gmail.com>
2023-08-18 21:51:40 -0300

    Consumo de API com perfis
    Perfis de usuarios adicionados a API - Aula 18/08

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import Home from "./pages/Home/";
 import ListaServicos from './pages/ListaServicos';
 import ListaDevs from './pages/ListaDevs';
 import Header from "../src/components/Header"
+import Footer from './components/Footer';
+import Perfil from "./pages/PerfilUsuario";
 
 //Rotas
 
@@ -14,7 +16,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 //estilização global
 import "./index.css";
-import Footer from './components/Footer';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -23,7 +25,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="lista/servicos" element={<ListaServicos/>}/>
-        <Route path='lista/devs' element={<ListaDevs/>}/>
+        <Route path="lista/devs" element={<ListaDevs/>}/>
+        {/* Rota com o parametro indicando o identificador do Desenvolvedor */}
+        <Route path="perfil/:idUsuario" element={<Perfil />}></Route>
       </Routes>
 
       <Footer />
